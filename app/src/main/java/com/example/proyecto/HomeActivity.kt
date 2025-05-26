04820c52953b62fe54157394e4d9a6c0af4a3af4
sapotronRonny <ronnychoez23@gmail.com>
2025-05-26 14:18:12 -0500

    cambio semana no se

--- app/src/main/java/com/example/proyecto/HomeActivity.kt
+++ app/src/main/java/com/example/proyecto/HomeActivity.kt
@@ -1,3 +1,22 @@
+package com.example.proyecto
+
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_home)
+    }
+}
+
 //package com.example.proyecto
 //
 //
